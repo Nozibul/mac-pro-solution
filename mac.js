79fc4fb2f5d321ba260bb683bd70ca3e563d4de0
Nozibul <nozibulislamspi@gmail,com>
2021-08-22 19:15:29 +0600

    finish code

--- mac.js
+++ mac.js
@@ -1,5 +1,4 @@
 
-// 
 const bestPrice = document.getElementById('best-price');
 const costMemory = document.getElementById('cost-memory');
 const storageCost = document.getElementById('storage-cost');
@@ -7,16 +6,19 @@ const deliveryCost = document.getElementById('delivery-cost');
 const cost = document.getElementById('total-cost');
 const finalTotal = document.getElementById('all-total');
 
-
+// All cost function
 function allTotalPrice() {
     cost.innerText = parseFloat(bestPrice.innerText) + parseFloat(costMemory.innerText) + parseFloat(storageCost.innerText) + parseFloat(deliveryCost.innerText);
     finalTotal.innerText = cost.innerText;
 };
+
+// total cost function
 function totalCost (product, cost){
     product.innerText = cost ;
     allTotalPrice() ;
 };
 
+// Add Even handler
 document.getElementById('gb8-memory').addEventListener('click', function(){
     totalCost(costMemory, 0);  
 });
@@ -39,8 +41,8 @@ document.getElementById('extra-delivery').addEventListener('click', function(){
     totalCost(deliveryCost, 20);  
 });
 
-// Promo code
 
+// Promo code
 const promoInputCode = document.getElementById('input-promo-code');
 const promoCode = document.getElementById('promo-code') ;
 promoCode.addEventListener('click', function(){
